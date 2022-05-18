4d9798b8e69fd7b2d055785f2ef0d7f1f1a63a83
billadaku <gurisingh12352@gmail.com>
2022-05-18 16:42:14 +0530

    font changed

--- src/comps/centerContent.js
+++ src/comps/centerContent.js
@@ -7,8 +7,8 @@ export default function Center() {
         <div className='text'>  
         <h1>Welcome To My Portfolio</h1>
         <br></br>
-        <p>Hello my name is Gursewak Singh from india and i am a mechanical engineer and i really like playing with code so i started learning
-          web dev . i have made this site using React and sass .but still it is not completed yet.
+        <p>Hello my name is Gursewak Singh from india and I am a mechanical engineer and I really like playing with code so I started learning
+          web dev . I have made this site using React and sass .But still it is not completed yet.
         </p>  
       
         <button><a href='https://discord.gg/SVQ2dDUxVP'>DISCORD</a></button>     
